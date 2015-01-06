024802038a39876981291716edd011f8c56b8c0e
JayTheBestMan <jaythebestman@hotmail.com>
2015-01-06 18:03:27 +0100

    Update kernel_approx_test.py

--- kernels/kernel_approx_test.py
+++ kernels/kernel_approx_test.py
@@ -7,14 +7,25 @@ x = 7
 sskn = 3
 sskl = 0.5
 
-#approxKern bor vara en 8x8-matris
-approxKern = kernel_approx.approxKernelSSK(a, n, x, sskn, sskl)
+update()
 
-#exactKern bor bara en 8x8-matrix
-exactKern = kernel_approx.exactGramSSK(a, sskn, sskl)
+def testString(S, nn, nx, nsskn, nsskl):
+	a = S
+	n = nn
+	x = nx
+	sskn = nsskn
+	sskl = nsskl
+	update()
 
-#align bor vara 0 < align < 1
-align = kernel_approx.alignment(approxKern, exactKern)
+def update():
+	#approxKern bor vara en 8x8-matris
+	approxKern = kernel_approx.approxKernelSSK(a, n, x, sskn, sskl)
 
-#alignExact bor vara = 1
-alignExact = kernel_approx.alignment(exactKern, exactKern)
+	#exactKern bor bara en 8x8-matrix
+	exactKern = kernel_approx.exactGramSSK(a, sskn, sskl)
+
+	#align bor vara 0 < align < 1
+	align = kernel_approx.alignment(approxKern, exactKern)
+
+	#alignExact bor vara = 1
+	alignExact = kernel_approx.alignment(exactKern, exactKern)
