cb883ac5c8ffad81b5cc0a2772e841d5d95982d3
cribalik <cmarte@kth.se>
2015-01-06 17:19:44 +0100

    Fixed error when SSK.py was called from the same folder it was in, because of weird p behaviour from python ;{)

--- kernels/SSK.py
+++ kernels/SSK.py
@@ -2,15 +2,15 @@ import subprocess
 from subprocess import Popen, PIPE
 import os.path
 
-__m_dir = os.path.dirname(__file__);
+__m_dir = os.path.dirname(os.path.abspath(__file__));
 
 
 def SSK(n, plambda, str1, str2):
     # Check if the kernel module has been compiled
-    if not os.path.isfile(os.path.join(__m_dir, "kernel-exact.out")):
-        print "calling 'make' to compile SSK kernel module"
-        subprocess.call(["make", "--directory=" + __m_dir, "kernel-exact.out"]);
+    # if not os.path.isfile(os.path.join(__m_dir, "kernel-exact.out")):
+        # print "calling 'make' to compile SSK kernel module"
 
+    subprocess.call(["make", "--directory=" + __m_dir, "kernel-exact.out"]);
     p = Popen([os.path.join(__m_dir, "kernel-exact.out"), str(n), str(plambda)], stdin=PIPE, stdout=PIPE);
 
     input = "\n".join([str(len(str1)), str1, str(len(str2)), str2]);
@@ -20,9 +20,9 @@ def SSK(n, plambda, str1, str2):
 
 def SSKfile(n, file1, file2, plambda=0.5):
     # Check if the kernel module has been compiled
-    if not os.path.isfile(os.path.join(__m_dir, "kernel-exact.out")):
-        print "calling 'make' to compile SSK kernel module"
-        subprocess.call(["make", os.path.join(__m_dir, "kernel-exact.out")]);
+    # if not os.path.isfile(os.path.join(__m_dir, "kernel-exact.out")):
+        # print "calling 'make' to compile SSK kernel module"
+    subprocess.call(["make", "--directory=" + __m_dir, "kernel-exact.out"]);
 
     s = subprocess.check_output([os.path.join(__m_dir, "kernel-exact.out"), str(n), file1, file2, str(plambda)]);
     return float(s);
