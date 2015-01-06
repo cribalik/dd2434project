46f6a207fcff29b3858ed3d0c25c867c16b9206b
JayTheBestMan <jaythebestman@hotmail.com>
2015-01-06 17:58:56 +0100

    Create kernel_approx_test.py

---
+++ kernels/kernel_approx_test.py
@@ -0,0 +1,20 @@
+import kernel_approx
+
+a = ["pengar", "mafksl", "kalfkwjkk", "fkalslfks", "akfpwrlk", "asldfpvl", "asldfa", "lfsasl"]
+
+n = 3
+x = 7
+sskn = 3
+sskl = 0.5
+
+#approxKern bor vara en 8x8-matris
+approxKern = kernel_approx.approxKernelSSK(a, n, x, sskn, sskl)
+
+#exactKern bor bara en 8x8-matrix
+exactKern = kernel_approx.exactGramSSK(a, sskn, sskl)
+
+#align bor vara 0 < align < 1
+align = kernel_approx.alignment(approxKern, exactKern)
+
+#alignExact bor vara = 1
+alignExact = kernel_approx.alignment(exactKern, exactKern)
