51a9ff010d261e7afdc0c3745771eda56a0e18b7
cribalik <cmarte@kth.se>
2015-01-06 19:59:47 +0100

    Hid output from 'make' command

--- kernels/SSK.py
+++ kernels/SSK.py
@@ -4,25 +4,27 @@ import os.path
 
 __m_dir = os.path.dirname(os.path.abspath(__file__));
 
-
+# Calculate SSK with parameters n and plambda for the strings str1 and str2
 def SSK(n, plambda, str1, str2):
     # Check if the kernel module has been compiled
-    # if not os.path.isfile(os.path.join(__m_dir, "kernel-exact.out")):
-        # print "calling 'make' to compile SSK kernel module"
-
-    subprocess.call(["make", "--directory=" + __m_dir, "kernel-exact.out"]);
+    if not os.path.isfile(os.path.join(__m_dir, "kernel-exact.out")):
+        print "calling 'make' to compile SSK kernel module"
+        with open(os.devnull, 'w') as devnull:
+            subprocess.call(["make", "--directory=" + __m_dir, "kernel-exact.out"], stdout=devnull, stderr=devnull);
+    
     p = Popen([os.path.join(__m_dir, "kernel-exact.out"), str(n), str(plambda)], stdin=PIPE, stdout=PIPE);
 
     input = "\n".join([str(len(str1)), str1, str(len(str2)), str2]);
     out, err = p.communicate(input);
     return float(out);
 
-
+# Calculates SSK with parameters n and plambda, where file1 och file2 are the two string for which to calculate the kernel for
 def SSKfile(n, file1, file2, plambda=0.5):
     # Check if the kernel module has been compiled
-    # if not os.path.isfile(os.path.join(__m_dir, "kernel-exact.out")):
-        # print "calling 'make' to compile SSK kernel module"
-    subprocess.call(["make", "--directory=" + __m_dir, "kernel-exact.out"]);
+    if not os.path.isfile(os.path.join(__m_dir, "kernel-exact.out")):
+        print "calling 'make' to compile SSK kernel module"
+        with open(os.devnull, 'w') as devnull:
+            subprocess.call(["make", "--directory=" + __m_dir, "kernel-exact.out"], stdout=devnull, stderr=devnull);
 
     s = subprocess.check_output([os.path.join(__m_dir, "kernel-exact.out"), str(n), file1, file2, str(plambda)]);
     return float(s);
