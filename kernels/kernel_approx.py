82f649c34807c7e601dc0ce14fe2ff0aa5d8c4a2
JayTheBestMan <jaythebestman@hotmail.com>
2015-01-08 13:42:11 +0100

    Update kernel_approx.py

--- kernels/kernel_approx.py
+++ kernels/kernel_approx.py
@@ -86,7 +86,17 @@ def approxKernelSSK(S, n, x, sskn, sskl):
 				tmp += SSK.SSK(sskn, sskl, S[i], st) * SSK.SSK(sskn, sskl, S[j], st)
 			ret[i,j] = tmp
 	return ret
-
+	
+def approxKernelSSKalt(S, n, x, sskn, sskl):
+	ret = np.zeros((len(S), len(S)))
+	for i in range(0, len(S)):
+		for j in range(0, len(S)):
+			tmp = 0
+			s = getSubset([S[i], S[j]], n, x)
+			for st in s:
+				tmp += SSK.SSK(sskn, sskl, S[i], st) * SSK.SSK(sskn, sskl, S[j], st)
+			ret[i,j] = tmp
+	return ret
 # Returnerar en exakt Gram-matris
 def exactGramSSK(S, sskn, sskl):
 	ret = np.zeros((len(S), len(S)))
