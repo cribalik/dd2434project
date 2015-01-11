3be8105c7ba76a9abf3933556df46360e146179f
pontused <pontus.edstrom@gmail.com>
2015-01-11 17:08:29 +0100

    fix av gram
    gram returnerar nu en rättvänd matris.
    test inehåller exempel på användning samt träning och användning av en
    svm

--- kernels/gram.py
+++ kernels/gram.py
@@ -1,11 +1,11 @@
 __author__ = 'Pontus'
 import numpy as np
 
-def gram(docs, kernel):
+def gram(docs1, docs2, kernel):
     g = []
-    for d1 in docs:
+    for d1 in docs2:
         g2 = []
-        for d2 in docs:
+        for d2 in docs1:
             g2.append(kernel(d1,d2))
         g.append(np.array(g2))
     return np.array(g)
