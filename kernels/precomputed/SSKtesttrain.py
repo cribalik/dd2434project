d9cda2a559d08f38b3e07fc043457ea20bc34204
cribalik <cmarte@kth.se>
2015-01-12 20:53:35 +0100

    File for calculating the Gram matrix for testing-training sets

---
+++ kernels/precomputed/SSKtesttrain.py
@@ -0,0 +1,44 @@
+from enum import Enum
+from SSK import SSKfile
+
+__author__ = "Christopher Martensson"
+
+class DataType(Enum):
+	training = 'training'
+	testing = 'testing'
+
+class Topics(Enum):
+    earn = 'earn'
+    acquisition = 'acq'
+    crude = 'crude'
+    corn = 'corn'
+
+count_dictionary = {
+    Topics.earn: {DataType.training: 152,
+                  DataType.testing: 40},
+    Topics.acquisition: {DataType.training: 114,
+                         DataType.testing: 25},
+    Topics.crude: {DataType.training: 76,
+                   DataType.testing: 15},
+    Topics.corn: {DataType.training: 38,
+                  DataType.testing: 10}
+}
+
+test_files = []
+training_files = []
+
+for topic in Topics:
+	for i in xrange( count_dictionary[topic][DataType.training] ):
+		training_files.append( '/home/cmarte/dd2434project/kernels/precomputed/training/' + topic.value + str(i) )
+
+for topic in Topics:
+  for i in xrange( count_dictionary[topic][DataType.testing] ):
+    test_files.append( '/home/cmarte/dd2434project/kernels/precomputed/testing/' + topic.value + str(i) )
+
+for length in [3,4,5,6,7,8,10,12,14]:
+	with open("SSK-2-length"+str(length)+".txt",'w') as f:
+		for file1 in test_files:
+			print file1
+			v = [str(SSKfile(length, file1, file2, 0.5)) for file2 in training_files]
+			f.write(','.join(v))
+			f.write('\n')
