e0162f175b42abe1e50e744d853a5500fcf6327f
Daniel Schlaug <daniel.schlaug@gmail.com>
2015-01-13 20:49:17 +0100

    Optimizations

--- kernels/ngk.py
+++ kernels/ngk.py
@@ -6,28 +6,39 @@ __author__ = 'Pontus'
 # n is the size of the n-grams used.
 # docstring1 and docstring2 are document strings with stop words and '.' removed
 # ngk(n)(docstring1, docstring2)
-def ngkernel(x, y, n):
-    # empty dict
-    d = {}
+def ngkernel(x, y, n, weighted=False):
+    if not weighted:
+        n_grams_in_x = set()
+        n_grams_in_y = set()
 
-    for i in range(len(x)):
-        d[x[i:i+n]] = 0.
+        for i in range(len(x)):
+            n_grams_in_x.add(x[i:i+n])
 
-    for i in range(len(y)):
-        d[y[i:i+n]] = 0.
+        for i in range(len(y)):
+            n_grams_in_y.add(y[i:i+n])
 
-    xd = d.copy()
-    yd = d.copy()
+        return len(n_grams_in_x.intersection(n_grams_in_y))
+    else:
+        n_grams_in_x = dict()
+        n_grams_in_y = dict()
 
-    #This only acounts for if the n-gram exist not how often Borde det vara xd[x[i:i+n]] = xd[x[i:i+n]] + 1 istället.
-    for i in range(len(x)):
-        xd[x[i:i+n]] = 1.
+        for i in range(len(x)):
+            n_gram = x[i:i+n]
+            if n_gram not in n_grams_in_x:
+                n_grams_in_x[n_gram] = 0
+            n_grams_in_x[n_gram] += 1
 
-    for i in range(len(y)):
-        yd[y[i:i+n]] = 1.
+        for i in range(len(y)):
+            n_gram = y[i:i+n]
+            if n_gram not in n_grams_in_y:
+                n_grams_in_y[n_gram] = 0
+            n_grams_in_y[n_gram] += 1
 
-    #return np.inner(np.array(xd.values()),np.array(yd.values()))
-    return np.dot(np.array(xd.values()),np.array(yd.values()).transpose())/len(yd.values())
+        n_grams_in_both = set(n_grams_in_x.keys()).intersection(set(n_grams_in_y.keys()))
+        res = 0
+        for n_gram in n_grams_in_both:
+            res += n_grams_in_y[n_gram] * n_grams_in_x[n_gram]
+        return res
 
 
 def ngk(n):
