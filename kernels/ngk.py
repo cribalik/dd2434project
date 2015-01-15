a294769cbbd70511ab035f6d825c926fcd14f33c
Daniel Schlaug <daniel.schlaug@gmail.com>
2015-01-15 23:21:34 +0100

    Should be union and fix nondeterminate values()

--- kernels/ngk.py
+++ kernels/ngk.py
@@ -39,12 +39,11 @@ def ngkernel(x, y, n, weighted=True):
                 n_grams_in_y[n_gram] = 0.
             n_grams_in_y[n_gram] += 1.
 
-        # This should really be the intersection, right? Since dimensions in which either vector is 0 should not count.
-        # (Not sure but I think this applies to the cosine distance as well.)
-        n_grams_from_both = set(n_grams_in_x.keys()).intersection(set(n_grams_in_y.keys()))
+        n_grams_from_both = set(n_grams_in_x.keys()).union(set(n_grams_in_y.keys()))
+        n_grams_from_both = list(n_grams_from_both)
 
-        weighted_x = [math.log(n_grams_in_y[key] + 1.) for key in n_grams_from_both]
-        weighted_y = [math.log(n_grams_in_y[key] + 1.) for key in n_grams_from_both]
+        weighted_x = [math.log(n_grams_in_x[key] + 1.) if key in n_grams_in_x else 0 for key in n_grams_from_both]
+        weighted_y = [math.log(n_grams_in_y[key] + 1.) if key in n_grams_in_y else 0 for key in n_grams_from_both]
 
         return 1 - spatial.distance.cosine(weighted_x, weighted_y)
 
