9c2a9ee9833e8366f77767c171ea3dafd3fd40db
Daniel Schlaug <daniel.schlaug@gmail.com>
2015-01-15 22:30:21 +0100

    Some cleaning and optimization of ngk

--- kernels/ngk.py
+++ kernels/ngk.py
@@ -1,7 +1,9 @@
 # coding=utf-8
-import numpy as np
 import math
+
 from scipy import spatial
+
+
 __author__ = 'Pontus'
 
 # n is the size of the n-grams used.
@@ -13,67 +15,38 @@ def ngkernel(x, y, n, weighted=True):
         n_grams_in_y = set()
 
         for i in range(len(x)):
-            n_grams_in_x.add(x[i:i+n])
+            n_grams_in_x.add(x[i:i + n])
 
         for i in range(len(y)):
-            n_grams_in_y.add(y[i:i+n])
+            n_grams_in_y.add(y[i:i + n])
 
         return len(n_grams_in_x.intersection(n_grams_in_y))
     else:
-        weighted_n_grams_in_x = dict()
-        weighted_n_grams_in_y = dict()
         n_grams_in_x = dict()
         n_grams_in_y = dict()
 
-        #set weights
+        # set weights
         for i in range(len(x)):
-            n_gram = x[i:i+n]
+            n_gram = x[i:i + n]
             if n_gram not in n_grams_in_x:
                 n_grams_in_x[n_gram] = 0.
             n_grams_in_x[n_gram] += 1.
 
-        #set weights
+        # set weights
         for i in range(len(y)):
-            n_gram = y[i:i+n]
+            n_gram = y[i:i + n]
             if n_gram not in n_grams_in_y:
                 n_grams_in_y[n_gram] = 0.
             n_grams_in_y[n_gram] += 1.
 
-        n_grams_from_both = set(n_grams_in_x.keys()).union(set(n_grams_in_y.keys()))
+        # This should really be the intersection, right? Since dimensions in which either vector is 0 should not count.
+        # (Not sure but I think this applies to the cosine distance as well.)
+        n_grams_from_both = set(n_grams_in_x.keys()).intersection(set(n_grams_in_y.keys()))
 
-        norm_y = 0.
-        norm_x = 0.
- 
-        for key in n_grams_from_both:
-            if key in n_grams_in_y:
-                weighted_n_grams_in_y[key] = (math.log(n_grams_in_y[key])+1.)
-            else:
-                weighted_n_grams_in_y[key] = 0.
-            norm_y += math.pow(weighted_n_grams_in_y[key], 2)
+        weighted_x = [math.log(n_grams_in_y[key] + 1.) for key in n_grams_from_both]
+        weighted_y = [math.log(n_grams_in_y[key] + 1.) for key in n_grams_from_both]
 
-            if key in n_grams_in_x:
-                weighted_n_grams_in_x[key] = (math.log(n_grams_in_x[key])+1.)
-            else:
-                weighted_n_grams_in_x[key] = 0.
-            #norm_x += math.pow(weighted_n_grams_in_x[key], 2)
-
-
-        ###
-        #dpne by spatial.distance.cosine
-        ###
-        #norm_y = math.sqrt(norm_y)
-        #norm_x = math.sqrt(norm_x)
-        #for key in n_grams_from_both:
-        #    if key in n_grams_in_y:
-        #        weighted_n_grams_in_y[key] /= norm_y
-        #    else:
-        #        weighted_n_grams_in_y[key] = 0.
-        #    if key in n_grams_in_x:
-        #        weighted_n_grams_in_x[key] /= norm_x
-        #    else:
-        #        weighted_n_grams_in_x[key] = 0.
-
-        return 1 - spatial.distance.cosine(weighted_n_grams_in_x.values(), weighted_n_grams_in_y.values())
+        return 1 - spatial.distance.cosine(weighted_x, weighted_y)
 
 
 def ngk(n):
