1f687b19bb015e6419b490a0e9c1f588fde56ba2
Daniel Schlaug <daniel.schlaug@gmail.com>
2015-01-16 16:28:15 +0100

    Added test that all docs are not orthogonal

--- kernels/test_ngkernel_IT.py
+++ kernels/test_ngkernel_IT.py
@@ -1,8 +1,24 @@
 from unittest import TestCase
+from dataset import Dataset, Topics
+from reuters import DataType
+from ngk import ngkernel
 
 __author__ = 'Daniel Schlaug'
 
 
 class TestNgkernelIT(TestCase):
     def test_ngkernel(self):
-        self.fail()
+        for topic in Topics:
+            dataset = Dataset()
+            training_bodies = [article.body for article in dataset.get_data(topic=topic, data_type=DataType.training)]
+            testing_bodies = [article.body for article in dataset.get_data(topic=topic, data_type=DataType.testing)]
+
+            evaluations = 0
+            perpendicular = 0
+            for test_body in testing_bodies:
+                for training_body in training_bodies:
+                    evaluations += 1
+                    if ngkernel(test_body, training_body, 14) == 1.0:
+                        perpendicular += 1
+
+            self.failIfEqual(evaluations, perpendicular)
