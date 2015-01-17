d12eee3e25f7cd1e4572307a94015b79758f1949
Daniel Schlaug <daniel.schlaug@gmail.com>
2015-01-17 12:28:54 +0100

    Added test for ngk

--- kernels/test_ngkernel_IT.py
+++ kernels/test_ngkernel_IT.py
@@ -1,15 +1,15 @@
 from unittest import TestCase
 from dataset import Dataset, Topics
 from reuters import DataType
-from ngk import ngkernel
+from kernels.ngk import ngkernel
 
 __author__ = 'Daniel Schlaug'
 
 
 class TestNgkernelIT(TestCase):
-    def test_ngkernel(self):
+    def test_ngkernel_orthogonal_data(self):
+        dataset = Dataset()
         for topic in Topics:
-            dataset = Dataset()
             training_bodies = [article.body for article in dataset.get_data(topic=topic, data_type=DataType.training)]
             testing_bodies = [article.body for article in dataset.get_data(topic=topic, data_type=DataType.testing)]
 
@@ -18,7 +18,27 @@ class TestNgkernelIT(TestCase):
             for test_body in testing_bodies:
                 for training_body in training_bodies:
                     evaluations += 1
-                    if ngkernel(test_body, training_body, 14) == 1.0:
+                    if ngkernel(test_body, training_body, 14) == 0.0:
                         perpendicular += 1
 
+            print((evaluations, perpendicular))
             self.failIfEqual(evaluations, perpendicular)
+
+    def test_ngkernel_orthogonal_data(self):
+        dataset = Dataset()
+        training_bodies = [article.body for article in dataset.get_data(topic=None, data_type=DataType.training)]
+        testing_bodies = [article.body for article in dataset.get_data(topic=None, data_type=DataType.testing)]
+
+        evaluations = 0
+        perpendicular = 0
+        for test_body in testing_bodies:
+            for training_body in training_bodies:
+                evaluations += 1
+                if ngkernel(test_body, training_body, 14) == 0.0:
+                    perpendicular += 1
+                    # print(test_body)
+                    # print(training_body)
+                    # print("")
+
+        print((evaluations, perpendicular))
+        self.failIfEqual(evaluations, perpendicular)
